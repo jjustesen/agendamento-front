23c37f8b4f6e36f2dc12afd07724636ea18586f5
jjustesen <johannesjustensen99@gmail.com>
2022-09-23 15:58:09 -0300

    Adiciona responsividade no dashboard

--- src/shared/pages/Dashboard/Dashboard.tsx
+++ src/shared/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,14 @@
-import { Flex, Grid, GridItem, Text } from '@chakra-ui/react'
+import { Flex, Grid, GridItem, Text, useMediaQuery } from '@chakra-ui/react'
 import React from 'react'
 import RstNavBar from 'shared/components/NavBar'
 import DashboardCalendar from './DashboardCalendar'
 
 const Dashboard = () => {
+  const [isMobile] = useMediaQuery('(min-width: 500px)')
+
   return (
     <Flex>
-      <RstNavBar />
+      {isMobile && <RstNavBar />}
       <Grid p={6} w="100%" templateColumns={{ base: '1fr', md: 'repeat(6, 1fr)' }} gap={6}>
         <GridItem colSpan={4} p={6} bg="white" borderRadius={16}>
           <Text fontWeight="bold" color="gray.700" fontSize="lg" mb={4}>
