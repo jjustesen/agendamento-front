3b615a4748fc181197edfb3bab649a90f92418b6
jjustesen <johannesjustensen99@gmail.com>
2022-09-25 01:27:48 -0300

    Adiciona skeleton em horarios

--- src/shared/pages/Dashboard/DashboardCalendarToday.tsx
+++ src/shared/pages/Dashboard/DashboardCalendarToday.tsx
@@ -1,19 +1,29 @@
-import { Grid } from '@chakra-ui/react'
+import { Grid, Skeleton } from '@chakra-ui/react'
 import React from 'react'
 import RstScheduleTime from 'shared/components/ScheduleTime'
-import { IHorariosControllerResponse } from 'shared/service/HorariosController'
+import { useUserContext } from 'shared/providers/user'
+import { useQueryHorariosControllerShow } from 'shared/service/HorariosController'
 
-interface iProps {
-  data: IHorariosControllerResponse[]
-}
+const DashboardCalendarToday = () => {
+  const { user } = useUserContext()
 
-const DashboardCalendarToday = ({ data }: iProps) => {
-  console.log(data)
+  const { data = [], isLoading } = useQueryHorariosControllerShow({ id_func: user?.id })
+
+  if (isLoading) {
+    return (
+      <Grid gap={4} overflow="auto" p={3}>
+        <Skeleton h={93} borderRadius={16} />
+        <Skeleton h={93} borderRadius={16} />
+        <Skeleton h={93} borderRadius={16} />
+      </Grid>
+    )
+  }
+
   return (
     <>
       <Grid gap={4} overflow="auto" p={3}>
         {data?.length ? (
-          data.map((item) => <RstScheduleTime key={item.id} item={item} color="whatsapp" />)
+          data.map((item) => <RstScheduleTime key={item.id} item={item} color={item.cliente ? 'whatsapp' : 'gray'} />)
         ) : (
           <p>Nenhum horário para hoje</p>
         )}
--- src/shared/pages/Dashboard/DashboardMobile.tsx
+++ src/shared/pages/Dashboard/DashboardMobile.tsx
@@ -1,20 +1,10 @@
 import { Collapse, Grid, GridItem, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
 import RstAccordion from 'shared/components/Accordion'
-import { useUserContext } from 'shared/providers/user'
-import { useQueryHorariosControllerShow } from 'shared/service/HorariosController'
 import DashboardCalendar from './DashboardCalendar'
 import DashboardCalendarToday from './DashboardCalendarToday'
 
 const DashboardMobile = () => {
-  const { user } = useUserContext()
-
-  console.log({ user })
-
-  const { data = [] } = useQueryHorariosControllerShow({ id_func: user?.id })
-
-  console.log(data)
-
   const { isOpen: isOpenToday, onToggle: onToggleToday } = useDisclosure()
   const { isOpen: isOpenCalendar, onToggle: onToggleCalendar } = useDisclosure()
 
@@ -24,7 +14,7 @@ const DashboardMobile = () => {
 
       <Collapse in={!!isOpenToday}>
         <GridItem p={1} bg="white" borderRadius={16} maxH="60vh" overflow="auto">
-          <DashboardCalendarToday data={data} />
+          <DashboardCalendarToday />
         </GridItem>
       </Collapse>
 
