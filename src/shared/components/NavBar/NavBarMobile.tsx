c2a6eeb9d060e803b5e87bcef7c5ce3f19d46b20
jjustesen <johannesjustensen99@gmail.com>
2022-09-24 21:45:32 -0300

    Corrige navbar mobile

--- src/shared/components/NavBar/NavBarMobile.tsx
+++ src/shared/components/NavBar/NavBarMobile.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react'
+import { Avatar, Box, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react'
 import React, { useState, useEffect } from 'react'
 import { iRoutes } from 'shared/interface/routes'
 import { FiMenu } from 'react-icons/fi'
@@ -41,34 +41,46 @@ const RstNavBarMobile = ({ routes }: iProps) => {
   }, [])
 
   return (
-    <Flex bg="white" justifyContent="space-between" alignItems="center" px={4} py={2}>
-      <Menu>
-        <MenuButton>
-          <Avatar name={user?.name} size="md" src={user?.avatar} />
-        </MenuButton>
-        <MenuList>
-          {users.map((user: iUser) => (
-            <MenuItem key={user.name} onClick={() => handleSelectUser(user)}>
-              <Avatar name={user.name} size="sm" src={user.avatar} mr={2} />
-              <Text>{user.name}</Text>
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
+    <>
+      <Box h={16} />
+      <Flex
+        position="fixed"
+        w="100vw"
+        bg="white"
+        justifyContent="space-between"
+        alignItems="center"
+        px={4}
+        py={2}
+        zIndex={9999}
+      >
+        <Menu>
+          <MenuButton>
+            <Avatar name={user?.name} size="md" src={user?.avatar} />
+          </MenuButton>
+          <MenuList>
+            {users.map((user: iUser) => (
+              <MenuItem key={user.name} onClick={() => handleSelectUser(user)}>
+                <Avatar name={user.name} size="sm" src={user.avatar} mr={2} />
+                <Text>{user.name}</Text>
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
 
-      <Text fontWeight="bold" fontSize="lg">
-        Agendaê
-      </Text>
+        <Text fontWeight="bold" fontSize="lg">
+          Agendaê
+        </Text>
 
-      <Menu>
-        <MenuButton>
-          <IconButton colorScheme="blue" size="lg" variant="ghost" aria-label="Menu" icon={<FiMenu />} />
-        </MenuButton>
-        <MenuList>
-          <MenuItem>Sair</MenuItem>
-        </MenuList>
-      </Menu>
-    </Flex>
+        <Menu>
+          <MenuButton>
+            <IconButton colorScheme="blue" size="lg" variant="ghost" aria-label="Menu" icon={<FiMenu />} />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Sair</MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+    </>
   )
 }
 
